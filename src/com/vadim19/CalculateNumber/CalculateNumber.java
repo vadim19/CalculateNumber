26acf48c32b340f8544919b59cc9d9a1c9998fa6
vadim19 <vreblyan@gmail.com>
2015-10-27 21:22:57 +0200

    Initial commit

---
+++ src/com/vadim19/CalculateNumber/CalculateNumber.java
@@ -0,0 +1,32 @@
+package com.vadim19.CalculateNumber;
+
+import java.util.Scanner;
+
+
+public class CalculateNumber {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int respounce;
+        double numberLow;
+        double numberHight;
+        double number;
+        respounce = 0;
+        numberLow = 1;
+        numberHight = 101;
+        while (respounce != 2) {
+            number = Math.floor((numberLow + numberHight) / 2);
+            System.out.println("Ваше число" + " " + number + "?");
+            respounce = Integer.parseInt(scanner.nextLine());
+            if (respounce == 2) {
+                System.out.println("Вітаємо");
+            } else if (respounce == 1) {
+                numberLow = number;
+            } else if (respounce == 0) {
+                numberHight = number;
+            }
+
+        }
+        return;
+    }
+
+}
